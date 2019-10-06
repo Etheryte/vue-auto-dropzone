788bdc4029933723cbb0186adea238a57ab9010e
Karl Tarvas <karl.tarvas@uptime.ee>
2019-10-07 02:29:15 +0800

    Init project

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript',
+  ],
+  rules: {
+    'no-console': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/comment-directive': 'off',
+    'semi': ['error', 'always'],
+    'space-before-function-paren': ['error', 'never'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'array-bracket-spacing': ['error', 'never', { 'objectsInArrays': false }],
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+  },
+  parserOptions: {
+    parser: "@typescript-eslint/parser"
+  },
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)"],
+      env: {
+        mocha: true
+      }
+    }
+  ]
+};
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
+</script>
