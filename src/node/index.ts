630ce28999bf29f62d5e20c923ef4bfdd1884fb0
Karl Tarvas <karl.tarvas@uptime.ee>
2019-10-07 09:41:30 +0800

    Further work on resolution

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,55 +1,61 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs').promises;
-const cleanupGlobal = require('jsdom-global')()
+
+// Dropzone requires a DOM context
+const jsdomCleanup = require('jsdom-global')()
 const Dropzone = require('dropzone');
 const libPath = require.resolve('dropzone');
 
+// Adaptation of https://flaviocopes.com/how-to-list-object-methods-javascript/
+function getDeepPropertyNames(input) {
+    let properties: Set<string> = new Set();
+
+    for (let current = input; current && current.constructor !== Object; current = Object.getPrototypeOf(current)) {
+        Object.getOwnPropertyNames(current).forEach(name => {
+            properties.add(name)
+        });
+    }
+
+    // Sorted simply for an easier debugging overview
+    return [...properties.keys()];
+}
+
 (async() => {
+    // TODO: Obsolete?
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     const libString = await fs.readFile(libPath, 'utf8');
 
     await page.evaluate(libString);
 
-    /*
-    const res = await page.evaluate(async () => {
-        // @ts-ignore
-        const instance = new window.Dropzone(document.body, {
-            url: '127.0.0.1',
-        });
-        const proto = Object.getPrototypeOf(instance);
-
-        // All the events the instance can emit
-        const eventNames = proto.events;
-
-        const methodNames = [];
-        const propertyNames = [];
-        const includeInternal = false;
-
-        Object.entries(proto).forEach(([key, value]) => {
-            // Internal values are prefixed with an underscore
-            if (!includeInternal && key.startsWith('_')) return;
-
-            if (typeof value === 'function') {
-                methodNames.push(key);
-            } else {
-                propertyNames.push(key);
-            }
-        });
-
-        return Object.keys(proto);
-    });
-
-    console.log(res);
-    */
-
+    // Create a placeholder instance to see what's exposed
     const instance = new Dropzone(document.body, {
+        // URL is a required parameter, just loop it back
         url: '127.0.0.1',
     });
-    const proto = Object.getPrototypeOf(instance);
+    const protoKeys = getDeepPropertyNames(instance);
 
-    console.log(instance.destroy);
+    // All the events the instance can emit
+    const eventNames = instance.events;
+    const methodNames: string[] = [];
+    const propertyNames: string[] = [];
+    const includeInternal = false;
+
+    protoKeys.forEach(key => {
+        // Internal values are prefixed with an underscore
+        if (!includeInternal && key.startsWith('_')) return;
+
+        const value = instance[key];
+        if (typeof value === 'function') {
+            methodNames.push(key);
+        } else {
+            propertyNames.push(key);
+        }
+    });
+
+    console.log('methods', methodNames);
+    console.log('props', propertyNames);
 
     await browser.close();
-    await cleanupGlobal();
+    await jsdomCleanup();
 })();
